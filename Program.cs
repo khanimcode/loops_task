1d777e13b6152928459b2332bf116fb2f767bc65
pshvaa <sabira.pshva@gmail.com>
2022-11-01 01:14:27 +0400

    created

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace intro3_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK-1
+            #region
+         //   int n = 10;
+         //   int sum = 0;
+         //   int count= 0;
+         //   for(int i = 1; i <= n; i++)
+         //   {
+         //       if (i % 2 == 0)
+         //       {
+         //           sum += i;
+         //           count++;
+                   
+         //       }
+         //   }
+         //float avarage   = sum / count;
+         //   Console.WriteLine(avarage);
+
+
+
+            #endregion
+            //TASK-2
+            #region
+            int num = 356;
+            int sum = 0;
+            int digit = 0;
+            while (num > 0)
+            {
+                digit = num % 10;
+                sum = sum + digit;
+                num = num / 10;
+
+            }
+            Console.WriteLine(sum);
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
